4dcc70b88ac15839d9d499a8ad368597dcd8c567
Shuvam Rauniyar <shubhamrauniyar48@gmail.com>
2023-10-03 00:59:25 +0530

    updated getFlights api in the repository layer

--- src/repository/flight-repository.js
+++ src/repository/flight-repository.js
@@ -1,6 +1,6 @@
-const {Flights} = require('../models/index');
+const {Flights,sequelize} = require('../models/index');
 const CrudRepository = require('./crud-repository');
-const {Op} = require('sequelize');
+const {Op,Sequelize} = require('sequelize');
 
 class FlightRepository extends CrudRepository{
     //for update flight i am using the update api from crud-repo
@@ -49,12 +49,33 @@ class FlightRepository extends CrudRepository{
         }
 
     }
+
     async getAllFlights(filter) {
         try{
-            const filterObject = this.#createFilter(filter);
-            const flight = await Flights.findAll({
-                where: filterObject
-            });
+            // let d = 'New Delhi';
+            // let b = 'butwal';
+            let getQuery = `select f.flightNumber,f.arrivalTime,f.departureTime,f.price from flights f 
+                        INNER JOIN airports a1 on a1.id = f.departureAirportId 
+                        INNER JOIN cities c1 on c1.id = a1.cityId 
+                        INNER JOIN airports a2 on a2.id = f.arrivalairportId 
+                        INNER JOIN cities c2 on c2.id = a2.cityId where c1.name = '${filter.source}' AND c2.name = '${filter.destination}' 
+                        AND Date_Format(f.departureTime,'%Y-%m-%d') = '${filter.date}'`;
+
+            if(filter.minPrice){
+                getQuery += ` AND f.price >= ${filter.minPrice}`
+            }
+            if(filter.maxPrice){
+                getQuery += ` AND f.price <= ${filter.maxPrice}`
+            }
+            const flight = await sequelize.query(getQuery, { type: Sequelize.QueryTypes.SELECT });
+
+            // const filterObject = this.#createFilter(filter);
+            // const flight = await Flights.findAll({
+            //     where: filterObject
+            // });
+
+            if(flight.length == 0)
+            return 'No flights found';
             return flight;
         }catch (error) {
             console.log("Something went wrong in the repository layer");
